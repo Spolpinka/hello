ed62129f3186277670434f1e03f48f63908c5d4c
Spolpinka <a421243266@gmail.com>
2022-07-28 21:17:49 +0300

    сделано ТЗ

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Приветище!!!");
+        System.out.println("Привет, Наставник ))!!!");
     }
 }
